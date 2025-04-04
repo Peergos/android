a986032f19cf24d1ba74ad0cbcaf05148336bc29
Ian Preston <ianopolous@protonmail.com>
2025-04-04 17:39:50 +0100

    Remove test classes which google play erroneous flags as as issue
    new version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 26
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
