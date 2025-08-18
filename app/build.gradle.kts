e3a86af1b4e28925959409e21402dc87af0785db
Ian Preston <ianopolous@protonmail.com>
2025-08-18 15:27:15 +0100

    v1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 30
         targetSdk = 35
-        versionCode = 21
-        versionName = "1.9.2"
+        versionCode = 22
+        versionName = "1.10.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
