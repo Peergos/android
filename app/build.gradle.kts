bfd5ecd299973a3a3e0d70f2660e8db1e5171f87
Ian Preston <ianopolous@protonmail.com>
2025-11-05 09:46:04 +0000

    Upgrade to latest Peergos and bump version to 1.14.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 30
         targetSdk = 35
-        versionCode = 26
-        versionName = "1.13.0"
+        versionCode = 27
+        versionName = "1.14.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
