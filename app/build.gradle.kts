3132475e65682277bcdc375276be7a1c9788c110
Ian Preston <ianopolous@protonmail.com>
2025-11-19 11:37:53 +0000

    v1.16.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 30
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.15.1"
+        versionCode = 30
+        versionName = "1.16.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
