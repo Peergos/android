83c71a934db114cb4952e564ccf96cbe6b7c558b
Ian Preston <ianopolous@protonmail.com>
2026-04-16 09:30:12 +0100

    v1.24.1 Fix sync

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 30
         targetSdk = 35
-        versionCode = 42
-        versionName = "1.24.0"
+        versionCode = 43
+        versionName = "1.24.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
