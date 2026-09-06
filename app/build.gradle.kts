3940041850ed6cbfc3bbfa0678f525de7aa8997c
Ian Preston <ianopolous@protonmail.com>
2026-09-06 11:41:29 +0100

    1.33.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "peergos.android"
         minSdk = 30
         targetSdk = 37
-        versionCode = 74
-        versionName = "1.33.4"
+        versionCode = 78
+        versionName = "1.33.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         externalNativeBuild {
